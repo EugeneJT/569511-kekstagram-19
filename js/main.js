6b8e12fe7e6a8441a1fa089ab52ff0aeb6418f7d
Eugene Nasonov <eugenetikk@gmail.com>
2020-02-13 00:39:24 +0300

    added the edits for the lesson 3

--- js/main.js
+++ js/main.js
@@ -10,6 +10,19 @@ var MESSAGES = ['Всё отлично!',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+var MIN_LIKES_VALUE = 15;
+var MAX_LIKES_VALUE = 200;
+var MIN_URL_VALUE = 1;
+var MAX_URL_VALUE = 6;
+var MIN_COMMENTS_VALUE = 1;
+var MAX_COMMENTS_VALUE = 5;
+var PICTURES_NUMBER = 25;
+
+
+var picturesContainer = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+
 var getRandomNum = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
@@ -20,7 +33,7 @@ var createCommentsArray = function (countComments) {
   for (var i = 0; i < countComments; i++) {
     array.push(
         {
-          avatar: 'img/avatar-' + getRandomNum(1, 6) + '.svg',
+          avatar: 'img/avatar-' + getRandomNum(MIN_URL_VALUE, MAX_URL_VALUE) + '.svg',
           message: getRandomNum(0, MESSAGES.length),
           name: getRandomNum(0, NAMES.length)
         }
@@ -35,36 +48,30 @@ var createPhotosArray = function (countPhotos) {
   for (var i = 0; i < countPhotos; i++) {
     array.push(
         {
-          url: 'photos/' + getRandomNum(1, 25) + '.jpg',
+          url: 'photos/' + (i + 1) + '.jpg',
           description: '',
-          likes: getRandomNum(15, 200),
-          comments: createCommentsArray(getRandomNum(1, 5))
+          likes: getRandomNum(MIN_LIKES_VALUE, MAX_LIKES_VALUE),
+          comments: createCommentsArray(getRandomNum(MIN_COMMENTS_VALUE, MAX_COMMENTS_VALUE))
         }
     );
   }
   return array;
 };
 
-var photos = createPhotosArray(25);
-
-var picturesContainer = document.querySelector('.pictures');
-
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var photos = createPhotosArray(PICTURES_NUMBER);
 
 var renderPicture = function (picture) {
   var pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('.picture__img').setAttribute('src', picture.url);
+  pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__likes').textContent = picture.likes + '';
   pictureElement.querySelector('.picture__comments').textContent = picture.comments.length + '';
 
   return pictureElement;
 };
 
-
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < photos.length; i++) {
   fragment.appendChild(renderPicture(photos[i]));
 }
 picturesContainer.appendChild(fragment);
-
