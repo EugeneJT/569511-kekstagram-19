8ef283458a5732b424b4feecd2b720df606dfa7a
Eugene Nasonov <eugenetikk@gmail.com>
2020-02-23 03:46:43 +0300

    added the module4-task3

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ var HASHTAGS_MAX_COUNT = 5;
 var HASHTAG = '#';
 
 var ESC_KEY = 27;
+var ENTER_KEY = 'Enter';
 
 var Scale = {
   MIN: 25,
@@ -66,6 +67,10 @@ var commentsCounter = document.querySelector('.social__comment-count');
 var commentsLoader = document.querySelector('.comments-loader');
 var bigPictureComment = bigPicture.querySelector('.social__comment');
 var bigPictureComments = bigPicture.querySelector('.social__comments');
+var bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+
+commentsCounter.classList.add('hidden');
+commentsLoader.classList.add('hidden');
 
 var getRandomNum = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
@@ -78,8 +83,8 @@ var createCommentsArray = function (countComments) {
     array.push(
         {
           avatar: 'img/avatar-' + getRandomNum(MIN_URL_VALUE, MAX_URL_VALUE) + '.svg',
-          message: getRandomNum(0, MESSAGES.length),
-          name: getRandomNum(0, NAMES.length)
+          message: MESSAGES[getRandomNum(0, MESSAGES.length - 1)],
+          name: NAMES[getRandomNum(0, NAMES.length - 1)],
         }
     );
   }
@@ -112,12 +117,6 @@ var renderPicture = function (picture) {
   pictureElement.querySelector('.picture__comments').textContent = picture.comments.length + '';
 
   return pictureElement;
-};
-
-// eslint-disable-next-line no-unused-vars
-var openPopupPreview = function () {
-  bigPicture.classList.remove('hidden');
-  bodyWrap.classList.add('modal-open');
 };
 
 
@@ -153,14 +152,51 @@ var renderBigPicture = function (photo) {
   bigPictureComments.appendChild(fragment);
 };
 
-
 renderBigPicture(photos[1]);
 
-commentsCounter.classList.add('hidden');
-commentsLoader.classList.add('hidden');
+
+var openPopupPreview = function () {
+  bigPicture.classList.remove('hidden');
+  bodyWrap.classList.add('modal-open');
+  document.addEventListener('keydown', onPopupEscPress);
+};
 
 
-// module4
+var closePopupPreview = function () {
+  bigPicture.classList.add('hidden');
+  bodyWrap.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var showBigPhoto = function (src) {
+  for (var i = 0; i < photos.length; i++) {
+    if (src === photos[i].url) {
+      openPopupPreview();
+      renderBigPicture(photos[i]);
+    }
+  }
+};
+
+var onPictureClick = function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    var activePicture = evt.target.attributes.src.value;
+    showBigPhoto(activePicture);
+  }
+};
+
+var onPictureEnterPress = function (evt) {
+  if (evt.key === ENTER_KEY) {
+    var activePicture = evt.target.children[0].attributes.src.value;
+    showBigPhoto(activePicture);
+  }
+};
+
+picturesContainer.addEventListener('click', onPictureClick);
+picturesContainer.addEventListener('keydown', onPictureEnterPress);
+bigPictureCancel.addEventListener('click', function () {
+  closePopupPreview();
+});
+
 
 var openModal = function () {
   bodyWrap.classList.add('modal-open');
